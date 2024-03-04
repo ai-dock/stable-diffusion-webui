05f9608c38b7a85510ccb65ba80eb866c90c7b5a
Rob Ballantyne <rob@dynamedia.uk>
2024-03-04 22:41:00 +0000

    Storagemonitor fix

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -1,4 +1,4 @@
-#!/bin/false
+#!/bin/bash
 # This file will be sourced in init.sh
 # Namespace functions with provisioning_
 
@@ -33,8 +33,8 @@ EXTENSIONS=(
 CHECKPOINT_MODELS=(
     "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
-    "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
-    "https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
+    #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
+    #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
 )
 
 LORA_MODELS=(
